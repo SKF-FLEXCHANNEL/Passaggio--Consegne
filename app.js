5b0d16e31477c9f726acd311a087939a7771e479
marconeri70 <marconeri70@gmail.com>
2026-05-22 19:17:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-// --- CONFIGURAZIONE ZONE E PUNTI ---
-// Dopo aver usato la calibrazione, incolla qui il JSON aggiornato
+// --- CONFIGURAZIONE ZONE E PUNTI (RIPRISTINATI) ---
 const zones = {
-  zona1: { title: 'ZONA 1 - Layout interattivo', img: 'img/zona1.jpg', points: [['SG01', 20, 23], ['OP30A', 14, 31]] },
-  zona2: { title: 'ZONA 2 - Layout interattivo', img: 'img/zona2.jpg', points: [] },
-  zona3: { title: 'ZONA 3 - Layout interattivo', img: 'img/zona3.jpg', points: [] }
+  zona1: { title: 'ZONA 1 - Layout interattivo', img: 'img/zona1.jpg', points: [
+    ['SG01', 20, 23], ['OP30A', 14, 31], ['OP30B', 35, 27], ['OP30C', 45, 25], ['OP30D', 53, 25], ['OP30E', 63, 27], ['SG03', 48, 36], ['SG05', 60, 37],
+    ['R01', 32, 47], ['OP10A', 28, 58], ['OP10B', 42, 57], ['R02', 42, 68], ['OP20A', 54, 62], ['OP20B', 54, 51], ['R03', 49, 42], ['OP40A', 66, 49], ['OP40B', 75, 49], ['OP60', 59, 58], ['OP70', 60, 65], ['R05', 59, 73], ['OP80', 70, 65], ['OP90', 77, 66], ['OP100', 83, 61], ['OP110', 84, 55], ['OP120', 77, 52], ['OP125', 86, 57], ['KEYENCE', 84, 66], ['SG02', 52, 84], ['Emergenza', 13, 86]
+  ]},
+  zona2: { title: 'ZONA 2 - Layout interattivo', img: 'img/zona2.jpg', points: [
+    ['Lavatrice SX', 18, 39], ['OP170A-B', 39, 47], ['R07', 40, 58], ['OP130', 51, 47], ['R08', 51, 58], ['OP140', 62, 47], ['R09', 61, 59], ['OP125C', 75, 51], ['OP135', 79, 46], ['OP150', 76, 59], ['OP200', 86, 39], ['SG01', 85, 28], ['SG02', 51, 82], ['Q.E. Zona 2', 58, 25], ['PH01 Emergenza', 51, 88]
+  ]},
+  zona3: { title: 'ZONA 3 - Layout interattivo', img: 'img/zona3.jpg', points: [
+    ['SG01', 13, 31], ['R10', 36, 38], ['R11', 35, 62], ['R12', 51, 64], ['R13', 55, 42], ['R14', 76, 44], ['OP230', 30, 49], ['OP240', 37, 49], ['OP250', 48, 49], ['OP260A', 51, 30], ['OP260B', 61, 30], ['OP270A', 50, 49], ['OP270B', 60, 49], ['OP290', 75, 60], ['SG03', 49, 31], ['SG05', 58, 31], ['SG07', 86, 37], ['Contenitore', 86, 61], ['Lavatrice', 13, 48], ['Q.E. Zona 3', 70, 25], ['SG02', 31, 84]
+  ]}
 };
 
-const KEY = 'passaggio_consegne_hmi_v1';
+const KEY = 'hmi_passaggio_consegne_v2';
 let currentZone = 'zona1';
 let selectedPoint = null;
 let anomalies = JSON.parse(localStorage.getItem(KEY) || '[]');
@@ -14,7 +20,7 @@ let calibrationMode = false;
 
 const $ = id => document.getElementById(id);
 
-// --- LOGICA DI CALIBRAZIONE (DRAG & DROP) ---
+// --- LOGICA DI CALIBRAZIONE ---
 function enableCalibration() {
     calibrationMode = true;
     console.log("MODE CALIBRAZIONE ATTIVO: Trascina i marker dove preferisci.");
@@ -176,5 +182,5 @@ $('exportBtn').onclick = () => {
 };
 
 // Start
-if ('serviceWorker' in navigator) navigator.serviceWorker.register('service-worker.js');
+if ('serviceWorker' in navigator) navigator.serviceWorker.register('service-worker.js').catch(()=>{});
 renderZone();
