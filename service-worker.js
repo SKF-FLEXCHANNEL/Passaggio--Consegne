7a5f3059549b227267879fa12ee3ef05ccc15d30
marconeri70 <marconeri70@gmail.com>
2026-05-26 11:37:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'passaggio-consegne-v21';
+const ASSETS = [
+  './index.html?v=21',
+  './style.css?v=21',
+  './app.js?v=21',
+  './manifest.json',
+  './img/zona1.jpg',
+  './img/zona2.jpg',
+  './img/zona3.jpg',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.includes('/api/')) return;
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(res => res || caches.match('./index.html?v=21'))
+    )
+  );
+});
